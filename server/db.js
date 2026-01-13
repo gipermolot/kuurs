66605142f5b0b520958dc4e03b843dc4ed20e59c
Alexandr <Alexandr@gmail.com>
2026-01-13 11:16:48 +0200

    prizma fix

--- server/db.js
+++ server/db.js
@@ -1,6 +1,3 @@
-import pkg from 'prisma/client';
-const { PrismaClient } = pkg;
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
-export default prisma;
+export const prisma = new PrismaClient();
